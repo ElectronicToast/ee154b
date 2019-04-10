2575abca1f523e60643bd7f82a5b5e1a77748409
David Elliott <delliott@caltech.edu>
2019-04-09 20:24:59 -0700

    Add PWM analog output to vary burn wire current

--- lab1/burnwire/burnwire.ino
+++ lab1/burnwire/burnwire.ino
@@ -1,6 +1,6 @@
 #define BURN_WIRE_PIN 3
-
-bool burn_wire_on = false;
+#define DUTY_MIN 0
+#define DUTY_MAX 255
 
 void setup() {
   // initialize serial:
@@ -8,12 +8,31 @@ void setup() {
   pinMode(BURN_WIRE_PIN, OUTPUT);
 }
 
+String str = "";
+
 void loop() {
-  while (Serial.available() == 0) {}
   while (Serial.available() > 0) {
-    Serial.read();
-    delay(10);
+    char ch = Serial.read();
+    if (isDigit(ch)) {
+      str += ch;
+    }
+    if (ch == '\n') {
+      if (!str.length()) {
+        Serial.println("Error: no valid digits sent");
+      } else {
+        int duty = str.toInt();
+        if (duty > DUTY_MAX || duty < DUTY_MIN) {
+          Serial.print("Error: duty must be between ");
+          Serial.print(DUTY_MIN);
+          Serial.print(" and ");
+          Serial.println(DUTY_MAX);
+        } else {
+          analogWrite(BURN_WIRE_PIN, duty);
+          Serial.print("Burn wire duty: ");
+          Serial.println(duty);
+        }
+        str = "";
+      }
+    }
   }
-  burn_wire_on = !burn_wire_on;
-  digitalWrite(BURN_WIRE_PIN, burn_wire_on);
 }
