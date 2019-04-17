7d477787dd44e304920ebcce81ca1337f15d1324
Evan Yeh <eyeh2809@yahoo.com>
2019-04-17 13:35:03 -0700

    Add main method

--- lab2/part1/loopback.py
+++ lab2/part1/loopback.py
@@ -1,53 +1,42 @@
 '''
-This program sends byte characters over serial port COM7 to an Arduino,
+This program sends byte characters over serial port to an Arduino,
 then prints what the Arduino sends back.
 '''
 
 from time import sleep
-import serial
+import 
 
-# Open the serial port on COM7 (windows)
-ser = serial.Serial('COM7', 9600)
+BAUDRATE = 9600
+PORT = 'COM8'
 
-# Confirm that the connection is established
-if ser.isOpen():
-  print('Serial port opened successfully.')
+def main():
+    # Open the serial port
+    ser = serial.Serial(PORT, BAUDRATE)
 
-# Continuously ask for input
-while True:
-  i = input(">> ")
-  if i == 'quit':
-    ser.close()
-    exit()
-  else:
-    out = ''
-    ser.write(bytes(i,'utf-8'))
-    sleep(0.1)
-    
+    # Confirm that the connection is established
+    if ser.isOpen():
+        print('Serial port opened successfully.')
+
+    # Continuously ask for input
+    while True:
+    i = input(">> ")
+    if i == 'quit':
+        ser.close()
+        exit()
+    else:
+        out = ''
+        ser.write(bytes(i,'utf-8'))
+        sleep(0.1)
+
     # in case there is more than 1 byte waiting for us, read all of them
     while ser.in_waiting > 0:
-      out += str(ser.read(1))
-    
+        out += str(ser.read(1))
+
     # don't print empty reads
     if out != '':
-      print(out)
-      
-      
-      
-      '''
-    try:
-        ser_bytes = ser.readline()
-        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
-    except:
-        print("Keyboard Interrupt")
-        break
+        print(out)
+
+if __name__ == '__main__':
+    main()
 
 
-for i in range(4):
-  c = input('hello?')
-  ser.write('1')
-  time.sleep(0.1)
-  print(ser.readline())
-  time.sleep(0.1)
-'''
