059457dcce39a82ed2122efffd0dd0dca65cf590
Ray Sun <rayjhsun@gmail.com>
2019-04-22 22:45:43 -0700

    Got test suite working

--- lab2/part3/Lab2_part3/Lab2_part3.ino
+++ lab2/part3/Lab2_part3/Lab2_part3.ino
@@ -1,14 +1,20 @@
-#define     BAUDRATE    9600
+#include <EEPROM.h>
 
+
+// Character constants
+#define     CHAR_NRST_RESET   '1'
 #define     CHAR_A      0x41
 #define     CHAR_Z      0x5A
 #define     CHAR_Zp     0x5B
 #define     CHAR_a      0x61
 #define     CHAR_z      0x7A
 #define     CHAR_zp     0x7B
-#define     CHAR_0      0x30
-#define     CHAR_9      0x39
-#include <EEPROM.h>
+
+
+// System constants
+#define     BAUDRATE    9600
+#define THIRTY_S_MS     30000
+
 
 // Logarithmic Thermistor calibration
 #define TMST_LOG_SLOPE -22.025
@@ -17,16 +23,33 @@
 // ADC Resolution
 const double ADC_RES = 1024.0;
 
+
+// EEPROM constants
+#define INT_SIZE          2
+// Size (bytes) of block of EEPROM for storing each temp reading
+#define TEMP_BLOCK_SIZE   6
+#define TEMP_2BLOCK_SIZE  12
+
+// Temperature logging constants
+#define TEMP_LOG_START_CHAR     '$'     // Logging start indicator
+#define TEMP_LOG_END_CHAR       '%'     // Logging end indicator
+
+// Temperature readback constants
+#define TEMP_SEP_CHAR     ','     // Value separator
+#define TEMP_EOM_CHAR     '#'     // Readback terminator
+
+
 const int TMST_PIN = 0;
 char c_rx;
 
-int resetaddr = 0;
-int tempaddr = 1;
-int lengthaddr = 511;
+const int resetaddr = 0;          // Reset stored at address 0
+#define TEMP_BLOCK_START      2
+int tempaddr = 2;                 // Blocks start at address 2
+const int lengthaddr = 510;       // Most recent block address stored at 511 | 510
 
 char input;
 
-byte resetcount;     // EEROM data in bytes
+unsigned int resetcount;          // EEROM #resets
 
 
 void setup() {
@@ -44,9 +67,10 @@ void loop() {
         delay(5);
         
         //****************************
-        // Clear reset counts
-        if(input == '!'){ 
+        // Clear reset counts if 'CHAR_NRST_RESET' entered
+        if(input == CHAR_NRST_RESET){ 
             EEPROM.write(resetaddr, 0);
+            EEPROM.write(resetaddr + 1, 0);
         }
         //****************************   
         else if(input == '0'){ //increment string
@@ -79,9 +103,9 @@ void loop() {
         }
         //***************************  
         else if(input == '4'){ //do a reset
-            resetcount = EEPROM.read(resetaddr);
+            EEPROM.get(resetaddr, resetcount);
             EEPROM.write(resetaddr, resetcount + 1);
-            Serial.print(resetcount);
+            Serial.print(resetcount);     // Print current # resets
             delay(5);     //prevent transmission corruption
             resetFunc();  //call reset
         }
@@ -92,32 +116,63 @@ void loop() {
                 double tmst_1_raw = (double) analogRead(TMST_PIN);
                 double tempc = calibrated(tmst_1_raw);
                 int writeaddr = tempaddr;
-          
-                EEPROM.write(writeaddr, t);
-                EEPROM.write(writeaddr+1, tempc);
-                EEPROM.write(lengthaddr, writeaddr);\
-                delay(30000);  
+
+                // Send start indicator
+                Serial.print(TEMP_LOG_START_CHAR);
+                
+                // While in block range
+                //    If all of EEPROM is used, not stored
+                if (writeaddr <= lengthaddr - TEMP_BLOCK_SIZE) {
+                    EEPROM.put(writeaddr, t);       
+                    EEPROM.put(writeaddr + INT_SIZE, tempc);
+                    EEPROM.put(lengthaddr, writeaddr);
+                    tempaddr += TEMP_BLOCK_SIZE;      // Advance to next address
+                }
+                
+                delay(1000);
+                //delay(THIRTY_S_MS);  
             }    
+            // Send end indicator
+            Serial.print(TEMP_LOG_START_CHAR);
         }
          //***************************
         else if(input == '7'){ //read timestamp and temp
-            int ptr = 1;
-            int writeaddr = EEPROM.read(lengthaddr);
+            int ptr = TEMP_BLOCK_START;
+            int writeaddr;
+            EEPROM.get(lengthaddr, writeaddr);
             while(ptr <= writeaddr){
-                EEPROM.read(ptr);
-                EEPROM.read(ptr+1);
-                ptr = ptr + 2;  
+                int t;
+                double temp;
+                EEPROM.get(ptr, t);
+                EEPROM.get(ptr + INT_SIZE, temp);
+                
+                Serial.print(t);
+                Serial.print(TEMP_SEP_CHAR);
+                Serial.print(temp);
+                Serial.print(TEMP_SEP_CHAR);
+                
+                ptr += TEMP_BLOCK_SIZE;           // Increment by block size
             }    
+            Serial.print(TEMP_EOM_CHAR);
         }
         //***************************
         else if(input == '8'){ //read every other timestamp and temp
-            int ptr = 1;
+            int ptr = TEMP_BLOCK_START;
             int writeaddr = EEPROM.read(lengthaddr);
             while(ptr <= writeaddr){
-                EEPROM.read(ptr);
-                EEPROM.read(ptr+1);
-                ptr = ptr + 4;  
-          }  
+                int t;
+                double temp;
+                EEPROM.get(ptr, t);
+                EEPROM.get(ptr + INT_SIZE, temp);
+                
+                Serial.print(t);
+                Serial.print(TEMP_SEP_CHAR);
+                Serial.print(temp);
+                Serial.print(TEMP_SEP_CHAR);
+                
+                ptr += TEMP_2BLOCK_SIZE;    // Increment by twice block size
+            }  
+            Serial.print(TEMP_EOM_CHAR);
         }
     //***************************
     }
