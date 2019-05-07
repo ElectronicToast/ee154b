4d03502ad380fcb967778dbdf180b012d50948a3
Evan Yeh <eyeh2809@yahoo.com>
2019-05-07 16:56:28 -0700

    Port code over to Arduino Uno because Due's UART is 3v3

--- project/klesh_relay/klesh_relay.ino
+++ project/klesh_relay/klesh_relay.ino
@@ -1,31 +1,40 @@
 /*
  * This sketch is meant to relay responses between a host computer and the LKM.
- * The computer connects to a Due's Serial port 0, while the LKM connects
- * to the Due's Serial port 1.
+ * The computer connects to an Uno's Serial port, while the LKM connects to
+ * a software serial port on pins 2 (RX) and 3 (TX).
  * 
- * Due Serial0:
- *      TX -> USB
- *      RX -> USB
- * Due Serial1:
- *      TX -> LKM RX
- *      RX -> LKM TX
+ * Uno Serial:
+ *      Pin 0 RX -> USB
+ *      Pin 1 TX -> USB
+ * Uno software serial
+ *      Pin 2 RX -> LKM TX
+ *      Pin 3 TX -> LKM RX
  */
+
+#include <SoftwareSerial.h>
 
 #define USB_BAUD 9600
 #define LKM_BAUD 9600
 
+#define SERIAL1_RX 2
+#define SERIAL1_TX 3
+
+SoftwareSerial LKMserial =  SoftwareSerial(SERIAL1_RX, SERIAL1_TX);
+
 void setup() {
+    pinMode(SERIAL1_RX, INPUT);
+    pinMode(SERIAL1_TX, OUTPUT);
     Serial.begin(USB_BAUD);
-    Serial1.begin(LKM_BAUD);
+    LKMserial.begin(LKM_BAUD);
 }
 
 void loop() {
     // if hear anything from computer, send it to LKM
     while (Serial.available() > 0) {
-        Serial1.print(Serial.read());
+        LKMserial.print(Serial.read());
     }
     // if hear anything from LKM, send it to computer
-    while (Serial1.available() > 0) {
-        Serial.print(Serial1.read());
+    while (LKMserial.available() > 0) {
+        Serial.print(LKMserial.read());
     }
 }
