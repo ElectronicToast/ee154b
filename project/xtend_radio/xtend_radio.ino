1c69613a34f7a61dfce0580ff3e74da010d66669
Evan Yeh <eyeh2809@yahoo.com>
2019-05-08 14:23:48 -0700

    Configure for two serial port use

--- project/xtend_radio/xtend_radio.ino
+++ project/xtend_radio/xtend_radio.ino
@@ -1,15 +1,33 @@
 
+#include <SoftwareSerial.h>
+
+#define USB_BAUD 9600
+#define LKM_BAUD 9600
+
+#define SERIAL1_RX 3
+#define SERIAL1_TX 4
+
+
 #define SHUTDOWN_PIN 2
 
+int counter = 0;
+
+SoftwareSerial LKMserial =  SoftwareSerial(SERIAL1_RX, SERIAL1_TX);
 
 void setup() {
+    pinMode(SERIAL1_RX, INPUT);
+    pinMode(SERIAL1_TX, OUTPUT);
     pinMode(SHUTDOWN_PIN, OUTPUT);
     digitalWrite(SHUTDOWN_PIN, HIGH);
-    Serial.begin(9600);
+    Serial.begin(USB_BAUD);
+    LKMserial.begin(LKM_BAUD);
 }
 
 void loop() {
-    while(Serial.available() > 0) {
-        Serial.print(Serial.read());
+    while(Serial.available()) {
+        LKMserial.write(Serial.read());
+    }
+    while(LKMserial.available()) {
+        Serial.write(LKMserial.read());
     }
 }
